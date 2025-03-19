3c610d8f6bfb6a6a583d82e48057f049da0e0101
Marcos Vinicius <marcos.carini22@gmail.com>
2025-03-19 19:29:24 -0300

    fix: change id type for OrderItemRepository

--- src/main/java/com/marcoscarini/course/repositories/OrderItemRepository.java
+++ src/main/java/com/marcoscarini/course/repositories/OrderItemRepository.java
@@ -2,7 +2,8 @@ package com.marcoscarini.course.repositories;
 
 import com.marcoscarini.course.entities.OrderItem;
 import com.marcoscarini.course.entities.User;
+import com.marcoscarini.course.entities.pk.OrderItemPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 }
