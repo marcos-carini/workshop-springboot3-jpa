ec3242e3c0e72439e5474cf06dc597cebd54b51c
Marcos Vinicius <marcos.carini22@gmail.com>
2025-03-21 15:06:37 -0300

    feat: add exception handling for delete

--- src/main/java/com/marcoscarini/course/services/UserService.java
+++ src/main/java/com/marcoscarini/course/services/UserService.java
@@ -2,8 +2,10 @@ package com.marcoscarini.course.services;
 
 import com.marcoscarini.course.entities.User;
 import com.marcoscarini.course.repositories.UserRepository;
+import com.marcoscarini.course.services.exceptions.DatabaseException;
 import com.marcoscarini.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +31,16 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            if (!repository.existsById(id)) {
+                throw new ResourceNotFoundException(id);
+            }
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+
+
     }
 
     public User update(Long id, User obj) {
